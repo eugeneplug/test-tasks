5cc86b004d0614d943bfe64c1003f9847b688aa0
eugeneplug <68537036+eugeneplug@users.noreply.github.com>
2025-02-01 13:25:52 +0300

    v2

--- task-1/script.js
+++ task-1/script.js
@@ -1,42 +1,6 @@
 document.getElementById('orderForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const phone = document.getElementById('phone').value;
-
-    const data = {
-        stream_code: 'vv4uf',
-        client: {
-            phone: phone,
-            name: name
-        }
-    };
-
-    fetch('https://order.drcash.sh/v1/order', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer RLPUUOQAMIKSAB2PSGUECA'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => {
-        if (response.ok) {
-            window.location.href = 'thankyou.html';
-        } else {
-            alert('Ошибка при отправке заявки');
-        }
-    })
-    .catch(error => {
-        console.error('Ошибка:', error);
-    });
-});
-
-
-
-document.getElementById('orderForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
     if (localStorage.getItem('formSubmitted')) {
         alert('Вы уже отправили заявку.');
         return;
